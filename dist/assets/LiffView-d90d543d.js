9050614206f6b3ade7428bad7486f29611306dca
cathy_wu <st12345686@gmail.com>
2023-08-19 01:27:49 +0800

    移除dist

--- dist/assets/LiffView-d90d543d.js
+++
@@ -1,1 +0,0 @@
-import{l as o,o as r,c}from"./index-de93e937.js";const s={__name:"LiffView",setup(t){return o.isLoggedIn()&&o.getProfile().then(e=>{console.log("profile",e)}).catch(e=>{console.log("error",e)}),(e,l)=>(r(),c("div"))}};export{s as default};
